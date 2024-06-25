ac0a253fd9c3d36c69882973dbcc1e3f063c877c
Hafiz Marham <hafiz.marham@dentsu.com>
2024-06-25 21:55:09 +0700

    add post message script

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,47 @@
 import '../styles/globals.css'
 import { useEffect } from 'react';
+import Script from 'next/script';
 
 function MyApp({ Component, pageProps }) {
 
-  useEffect(() => {
-    window.addEventListener("message", function (event) {
-      // We are receiveing messages from any origin, you can check of the origin by
-      // using event.origin
+  // useEffect(() => {
+  //   window.addEventListener("message", function (event) {
+  //     // We are receiveing messages from any origin, you can check of the origin by
+  //     // using event.origin
     
-      // get the port then use it for communication.
-      var port = event.ports[0];
-      if (typeof port === 'undefined') return;
+  //     // get the port then use it for communication.
+  //     var port = event.ports[0];
+  //     if (typeof port === 'undefined') return;
     
-      // Post message on this port.
-      port.postMessage("Test")
+  //     // Post message on this port.
+  //     port.postMessage("Test")
     
-      // Receive upcoming messages on this port.
-      port.onmessage = function(event) {
-        console.log("[PostMessage1] Got message" + event.data);
-      };
-    });
-  })
+  //     // Receive upcoming messages on this port.
+  //     port.onmessage = function(event) {
+  //       console.log("[PostMessage1] Got message" + event.data);
+  //     };
+  //   });
+  // })
+  <Script>
+    {`
+      window.addEventListener("message", function (event) {
+        // We are receiveing messages from any origin, you can check of the origin by
+        // using event.origin
+      
+        // get the port then use it for communication.
+        var port = event.ports[0];
+        if (typeof port === 'undefined') return;
+      
+        // Post message on this port.
+        port.postMessage("Test")
+      
+        // Receive upcoming messages on this port.
+        port.onmessage = function(event) {
+          console.log("[PostMessage1] Got message" + event.data);
+        };
+      })
+    `}
+  </Script>
 
   return <Component {...pageProps} />
 }
